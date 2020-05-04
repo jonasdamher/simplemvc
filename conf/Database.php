96fe34b2898882f5d6bf3ba3547170b206dbb63f
Jonás <jonas.damher@gmail.com>
2020-05-04 11:42:29 +0100

    sintax Database class

--- conf/Database.php
+++ conf/Database.php
@@ -1,6 +1,7 @@
 <?php
 
-class Database {
+class Database
+{
 
     private static $driver = 'mysql';
     private static $dns = 'localhost';
@@ -12,31 +13,29 @@ class Database {
 
     private static $connection = null;
 
-    private static function connectionTo(){
+    private static function connectionTo()
+    {
         try {
-            $dns = self::$driver.':host='.self::$dns.'; port='.self::$port.'; dbname='.self::$databaseName.'; charset='.self::$charset;
+            $dns = self::$driver . ':host=' . self::$dns . '; port=' . self::$port . '; dbname=' . self::$databaseName . '; charset=' . self::$charset;
             $pdoConnection = new PDO($dns, self::$userName, self::$password);
             $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdoConnection;
-
         } catch (PDOException $e) {
             die('Error connect to database: ' . $e->getMessage());
         }
     }
 
-    public static function connect() {
-       
-        if(self::$connection == null){
+    public static function connect()
+    {
+        if (self::$connection == null) {
             self::$connection = self::connectionTo();
         }
-
         return self::$connection;
     }
 
-    public static function disconnect() {
+    public static function disconnect()
+    {
         self::$connection = null;
     }
-
 }
-
 ?>
