4cf095bcd789bbac47f7e07ac9b0b99ca59355cb
Jonás <jonas.damher@gmail.com>
2020-05-11 12:47:36 +0100

    update database.php

--- conf/Database.php
+++ conf/Database.php
@@ -1,11 +1,12 @@
 <?php
+
 class Database
 {
 
     private static $driver = 'mysql';
     private static $dns = 'localhost';
     private static $port = '3008';
-    private static $databaseName = 'example';
+    private static $databaseName = 'simplymvcphp';
     private static $charset = 'utf8mb4';
     private static $userName = 'root';
     private static $password = '';
@@ -15,9 +16,11 @@ class Database
     private static function connectionTo()
     {
         try {
+
             $dns = self::$driver . ':host=' . self::$dns . '; port=' . self::$port . '; dbname=' . self::$databaseName . '; charset=' . self::$charset;
             $pdoConnection = new PDO($dns, self::$userName, self::$password);
             $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             return $pdoConnection;
         } catch (PDOException $e) {
             die('Error connect to database: ' . $e->getMessage());
