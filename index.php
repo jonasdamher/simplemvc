ed8bf6064d0ce4fa7193ef65bb0f16dbefafd9ab
Jonás <jonas.damher@gmail.com>
2020-05-11 12:43:06 +0100

    update index.php

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ require_once 'libs/Utils.php';
 require_once 'libs/autoload.php';
 
 require_once 'conf/Database.php';
-require_once 'models/BaseModel.php';
-require_once 'controllers/BaseController.php';
+require_once 'core/BaseModel.php';
+require_once 'core/BaseController.php';
 
 require_once 'libs/Router.php';
 
 $router = new Router();
+
 ?>
