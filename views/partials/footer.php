16d3924c655e36b30cf91d034783899d848f7e44
Jonás <jonas.damher@gmail.com>
2020-05-11 12:49:48 +0100

    update footer and head

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,9 +1,10 @@
 <footer class="footer">
 	<div class="footer-body">
-	<p>simplymvcphp created by jonasdamher.</p>
+		<p>simplymvcphp created by jonasdamher.</p>
 	</div>
 </footer>
 <!-- SCRIPT -->
+<script src="<?= URL_BASE ?>/public/js/dom.js" defer></script>
 <script src="<?= URL_BASE ?>/public/js/main.js" defer></script>
 </body>
 
--- views/partials/head.php
+++ views/partials/head.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <title><?= $this->currentView['section'] ?> - simplymvcphp</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Example page, using PHP with MVC architecture" />
-    <title>simplymvcphp - <?=$this->currentView['section']?></title>
-    <link rel="shortcut icon" type="image/png" href="<?= URL_BASE ?>public/img/favicon-simplymvcphp.png"/>
-    <link rel="stylesheet" type="text/css" href="<?=URL_BASE ?>public/css/main.css" />
+    <link rel="shortcut icon" type="image/png" href="<?= URL_BASE ?>public/img/favicon-simplymvcphp.png" />
+    <link rel="stylesheet" type="text/css" href="<?= URL_BASE ?>public/css/main.css" />
     <script src="<?= URL_BASE ?>public/js/jquery-3.4.1.min.js"></script>
     <script src="<?= URL_BASE ?>public/js/ckeditor/ckeditor.js"></script>
 </head>
+
 <body>
