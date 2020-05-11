dccc2d2f002a077345c394084a78f36207f7ee00
Jonás <jonas.damher@gmail.com>
2020-05-11 12:46:55 +0100

    add baseModel, baseController

---
+++ core/BaseController.php
@@ -0,0 +1,74 @@
+<?php
+
+class BaseController
+{
+
+	protected array $currentView = [];
+	private string $responseModel = '';
+	private array $models = [];
+
+	protected function setResponseModel(string $res)
+	{
+		$this->responseModel = $res;
+	}
+
+	public function getResponseModel(): string
+	{
+		return $this->responseModel;
+	}
+
+	protected function loadModels(array $modelsName)
+	{
+		foreach ($modelsName as $modelName) {
+			$model = ucfirst($modelName) . 'Model';
+
+			if (file_exists('models/' . $model . '.php')) {
+				require_once 'models/' . $model . '.php';
+				$this->models[$modelName] = new $model();
+			}
+		}
+	}
+
+	protected function model(string $modelName): object
+	{
+		return $this->models[$modelName];
+	}
+
+	protected function view(string $controller, string $action = 'index'): string
+	{
+		if (!is_dir('views/' . $controller) || !is_file('views/' . $controller . '/' . $action . '.php')) {
+			Utils::redirection('error/error500');
+		}
+
+		$this->currentView['section'] = $controller;
+		$this->currentView['view'] = $action;
+
+		return 'views/index.php';
+	}
+
+	protected function submitForm(): bool
+	{
+		return ($_SERVER['REQUEST_METHOD'] == 'POST');
+	}
+
+	protected function auth(string $role = null)
+	{
+		switch ($role) {
+			case 'ROLE_ADMIN':
+				if (!isset($_SESSION['userInit'])) {
+					Utils::redirection('home');
+				}
+
+				if ($_SESSION['userIdRol'] != 1 || $_SESSION['userRolIdentity'] != 'ROLE_ADMIN') {
+					Utils::redirection('home');
+				}
+				break;
+			default:
+				if (isset($_SESSION['userInit'])) {
+					Utils::redirection('users/profile');
+				}
+				break;
+		}
+	}
+}
+?>
---
+++ core/BaseModel.php
@@ -0,0 +1,110 @@
+<?php
+
+class BaseModel
+{
+
+	protected string $table = '';
+
+	private array $response = [];
+
+	public function __construct(string $table)
+	{
+		$this->table = $table;
+	}
+
+	protected function success(?array $result, $message = 'Success')
+	{
+		$this->response = ['success' => true, 'message' => $message, 'result' => $result];
+	}
+
+	protected function fail(string $message)
+	{
+		$this->response = ['success' => false, 'message' => $message];
+	}
+
+	protected function response(): array
+	{
+		return $this->response;
+	}
+
+	protected function find(): array
+	{
+		try {
+
+			$consult = Database::connect()->prepare("SELECT * FROM $this->table ORDER BY id DESC");
+			$consult->execute();
+
+			if ($consult->rowCount() == 0) {
+				throw new Exception("Don't exist ");
+			}
+
+			$result = $consult->fetchAll(PDO::FETCH_ASSOC);
+
+			$this->success($result);
+		} catch (Exception $e) {
+
+			$this->fail($e->getMessage());
+		} catch (PDOException $e) {
+
+			$this->fail($e->getMessage());
+		} finally {
+
+			$consult = null;
+			Database::disconnect();
+			return $this->response();
+		}
+	}
+
+	protected function findById(int $id): array
+	{
+		try {
+
+			$consult = Database::connect()->prepare("SELECT * FROM $this->table WHERE id=:id");
+
+			$consult->bindValue(':id', $id, PDO::PARAM_STR);
+
+			$consult->execute();
+
+			if ($consult->rowCount() == 0) {
+				throw new Exception('Not found');
+			}
+
+			$result = $consult->fetch(PDO::FETCH_ASSOC);
+
+			$this->success($result);
+		} catch (Exception $e) {
+
+			$this->fail($e->getMessage());
+		} catch (PDOException $e) {
+
+			$this->fail($e->getMessage());
+		} finally {
+
+			$consult = null;
+			Database::disconnect();
+			return $this->response();
+		}
+	}
+
+	protected function deleteById(int $id): array
+	{
+		try {
+
+			$consult = Database::connect()->prepare("DELETE FROM $this->table WHERE id = :id");
+
+			$consult->bindValue(':id', $id, PDO::PARAM_INT);
+
+			$consult->execute();
+
+			$this->success(null, 'Deleted');
+		} catch (PDOException $e) {
+
+			$this->fail($e->getMessage());
+		} finally {
+
+			$consult = null;
+			Database::disconnect();
+			return $this->response();
+		}
+	}
+}
