924e83916b336752c626425e2f95c55794973dc3
Jonás <jonas.damher@gmail.com>
2020-06-22 21:51:29 +0100

    cambio nombre método carga de modelos, propiedad nueva añadida y método para carga de clase JsonRequest para uso en clases hijas

--- core/BaseController.php
+++ core/BaseController.php
@@ -8,6 +8,7 @@ class BaseController
 	protected array $currentView = [];
 	private string $responseModel = '';
 	private array $models = [];
+	private ?object $jsonReq = null;
 
 	protected function setResponseModel(string $res)
 	{
@@ -19,21 +20,33 @@ class BaseController
 		return $this->responseModel;
 	}
 
-	protected function loadModels(array $modelsName)
+	public function modelLoading(array $modelsName)
 	{
 		foreach ($modelsName as $modelName) {
 			$model = ucfirst($modelName) . 'Model';
 
 			if (file_exists('models/' . $model . '.php')) {
 				require_once 'models/' . $model . '.php';
-				$this->models[$modelName] = new $model();
+				$this->models[$modelName] = new $model;
 			}
 		}
 	}
 
-	protected function model(string $modelName): object
+	public function model(string $modelName): object
 	{
 		return $this->models[$modelName];
+	}
+
+	/**
+	 * Para recoger respuestas o enviar json
+	 */
+	public function json(): object
+	{
+		if (is_null($this->jsonReq)) {
+			$this->jsonReq = new JsonRequest;
+		}
+
+		return $this->jsonReq;
 	}
 
 	protected function view(string $controller, string $action = 'index'): string
@@ -45,8 +58,6 @@ class BaseController
 		$this->currentView['section'] = $controller;
 		$this->currentView['view'] = $action;
 
-		Head::setTitle($action == 'index' ? $controller : $action);
-
 		return 'views/index.php';
 	}
 
@@ -55,20 +66,18 @@ class BaseController
 		return ($_SERVER['REQUEST_METHOD'] == 'POST');
 	}
 
-	protected function auth(string $role = null)
+	public function auth(string $role = null)
 	{
 		switch ($role) {
 			case 'ROLE_ADMIN':
-				if (!isset($_SESSION['userInit'])) {
+
+				if (!isset($_SESSION['userId']) || $_SESSION['userIdRol'] != 1 || $_SESSION['userRolIdentity'] != 'ROLE_ADMIN') {
 					Utils::redirection('home');
 				}
 
-				if ($_SESSION['userIdRol'] != 1 || $_SESSION['userRolIdentity'] != 'ROLE_ADMIN') {
-					Utils::redirection('home');
-				}
 				break;
 			default:
-				if (isset($_SESSION['userInit'])) {
+				if (isset($_SESSION['userId'])) {
 					Utils::redirection('users/profile');
 				}
 				break;
