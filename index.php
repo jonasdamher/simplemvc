41df3c118fcf8e14be7d748f0d14f452b6616474
Jonás <jonas.damher@gmail.com>
2020-06-25 19:48:09 +0100

    comentarios añadidos a index

--- index.php
+++ index.php
@@ -4,20 +4,21 @@ error_reporting(E_ALL);
 
 session_start();
 
+// Variables globales
 require_once 'conf/Globals.php';
 
-require_once 'libs/JsonRequest.php';
-
-require_once 'libs/Head.php';
-require_once 'libs/Utils.php';
+// Autocargar todas las clases
 require_once 'libs/autoload.php';
 
+// Clases de ayuda
+require_once 'libs/JsonRequest.php';
+require_once 'libs/Head.php';
+require_once 'libs/Utils.php';
+
+// Clases principales de la webapp
 require_once 'core/Database.php';
 require_once 'core/BaseModel.php';
 require_once 'core/BaseController.php';
 
+// Router
 require_once 'libs/Router.php';
-
-$router = new Router();
-
-?>
