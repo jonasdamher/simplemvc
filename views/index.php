0d04eca8384c09b24118c1e47d5b4a1d3016fcfb
Jonás <jonas.damher@gmail.com>
2020-06-25 19:49:02 +0100

    comentarios añadidos index de views

--- views/index.php
+++ views/index.php
@@ -1,11 +1,22 @@
 <?php
 include 'partials/head.php';
 
+/**
+ * Incluye una barra de navegación distinta si estás iniciado sesión o no
+ */
 include 'partials/' . (empty($_SESSION) ? 'navbar' : 'navbarAdmin') . '.php';
 
 ?>
 <main>
-	<?php include $this->currentView['section'] . '/' . $this->currentView['view'] . '.php'; ?>
+<?php
+
+include $this->currentView['section'] . '/' . $this->currentView['view'] . '.php';
+
+?>
 </main>
 
-<?php include 'partials/footer.php'; ?>
+<?php
+
+include 'partials/footer.php';
+
+?>
