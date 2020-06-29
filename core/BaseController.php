04a0e5051b938389f308129952c29a17ad66d94d
Jonás <jonas.damher@gmail.com>
2020-06-29 23:10:59 +0100

    modularización de y pequeña mejora al separar comprobación de permisos de usuario

--- core/BaseController.php
+++ core/BaseController.php
@@ -4,26 +4,44 @@ declare(strict_types=1);
 
 /**
  * Controlador base, administra los modelos, 
- * vistas, permisos de usuario y peticiones formato JSON
+ * vistas, permisos de usuario y peticiones formato JSON.
  */
 class BaseController
 {
 
 	protected array $currentView = [];
-	private string $responseModel = '';
 	private array $models = [];
-	private ?object $jsonReq = null;
+	private string $responseModel = '';
 
+	// Otros módulos.
+	protected ?JsonRequest $json = null;
+	protected ?Auth $auth = null;
+	
+	public function __construct()
+	{
+		$this->auth = new Auth;
+		$this->jsonReq = new JsonRequest;
+	}
+
+	/**
+	 * Para guardar la respuesta de un método de un modelo.
+	 */
 	protected function setResponseModel(string $res)
 	{
 		$this->responseModel = $res;
 	}
 
+	/**
+	 * Para mostrar respuesta guardada de un modelo.
+	 */
 	public function getResponseModel(): string
 	{
 		return $this->responseModel;
 	}
 
+	/**
+	 * Cargar un modelo o varios a la vez.
+	 */
 	public function modelLoading(array $modelsName)
 	{
 		foreach ($modelsName as $modelName) {
@@ -36,23 +54,17 @@ class BaseController
 		}
 	}
 
+	/**
+	 * Te devuelve una instancia del modelo especificado por parámetro.
+	 */
 	public function model(string $modelName): object
 	{
 		return $this->models[$modelName];
 	}
 
 	/**
-	 * Para solicitudes JSON.
+	 * Permite devuelver una vista.
 	 */
-	public function json(): object
-	{
-		if (is_null($this->jsonReq)) {
-			$this->jsonReq = new JsonRequest;
-		}
-
-		return $this->jsonReq;
-	}
-
 	protected function view(string $controller, string $action = 'index'): string
 	{
 		if (!is_dir('views/' . $controller) || !is_file('views/' . $controller . '/' . $action . '.php')) {
@@ -65,26 +77,12 @@ class BaseController
 		return 'views/index.php';
 	}
 
+	/**
+	 * Comprueba que exista una petición de tipo POST
+	 */
 	protected function submitForm(): bool
 	{
 		return ($_SERVER['REQUEST_METHOD'] == 'POST');
 	}
 
-	public function auth(string $role = null)
-	{
-		switch ($role) {
-			case 'ROLE_ADMIN':
-
-				if (!isset($_SESSION['userId']) || $_SESSION['userIdRol'] != 1 || $_SESSION['userRolIdentity'] != 'ROLE_ADMIN') {
-					Utils::redirection('home');
-				}
-
-				break;
-			default:
-				if (isset($_SESSION['userId'])) {
-					Utils::redirection('users/profile');
-				}
-				break;
-		}
-	}
 }
