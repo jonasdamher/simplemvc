6923d428d45abc193da1de959074d99ca549ce82
Jonás <jonas.damher@gmail.com>
2020-06-29 22:27:13 +0100

    credenciales de ejemplo para conexión a mas de una DDBB

--- conf/credentials.php
+++ conf/credentials.php
@@ -6,11 +6,23 @@ declare(strict_types=1);
  * Credenciales a la DB
  */
 return [
-	'driver' => 'mysql',
-	'dns' => 'localhost',
-	'port' => 3008,
-	'databaseName' => 'simplymvcphp',
-	'charset' => 'utf8mb4',
-	'userName' => 'root',
-	'password' => ''
+	'default' => [
+		'driver' => 'mysql',
+		'dns' => 'localhost',
+		'port' => 3008,
+		'databaseName' => 'simplymvcphp',
+		'charset' => 'utf8mb4',
+		'userName' => 'root',
+		'password' => ''
+	],
+	'helpdesk' => [
+		'driver' => 'mysql',
+		'dns' => 'localhost',
+		'port' => 3008,
+		'databaseName' => 'helpdeskdb',
+		'charset' => 'utf8mb4',
+		'userName' => 'root',
+		'password' => ''
+	]
+
 ];
