92fc725c38cfaacca670b1328e3921474563c891
Jonás <jonas.damher@gmail.com>
2020-06-29 22:24:01 +0100

    modificación para conexion simultanea a varias DDBB

--- core/Database.php
+++ core/Database.php
@@ -2,71 +2,39 @@
 
 declare(strict_types=1);
 
+require_once 'core/DatabaseHandler.php';
+
 /**
  * Clase para establecer conexión a la base de datos.
  */
-class Database
+class Database extends DatabaseHandler
 {
-    private static string $driver = '';
-    private static string $dns = '';
-    private static int $port = 0;
-    private static string $databaseName = '';
-    private static string $charset = '';
-    private static string $userName = '';
-    private static string $password = '';
-
-    private static ?object $connection = null;
-
-    /**
-     * Añadir credenciales para conexión a la base de datos.
-     */
-    private static function credentials()
-    {
-        $credentials = require_once 'conf/credentials.php';
-
-        self::$driver = $credentials['driver'];
-        self::$dns = $credentials['dns'];
-        self::$port = $credentials['port'];
-        self::$databaseName = $credentials['databaseName'];
-        self::$charset = $credentials['charset'];
-        self::$userName = $credentials['userName'];
-        self::$password = $credentials['password'];
-    }
-
-    /**
-     * Iniciar la conexión a la base de datos.
-     */
-    private static function connectionTo(): object
-    {
-        try {
-            self::credentials();
-
-            $dns = self::$driver . ':host=' . self::$dns . '; port=' . self::$port . '; dbname=' . self::$databaseName . '; charset=' . self::$charset;
-            $pdoConnection = new PDO($dns, self::$userName, self::$password);
-            $pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            return $pdoConnection;
-        } catch (PDOException $e) {
-            die('Error connect to database: ' . $e->getMessage());
-        }
-    }
-
     /**
      * Conectar a la DB.
      */
-    public static function connect(): object
+    public static function connect($db = null): object
     {
-        if (self::$connection == null) {
-            self::$connection = self::connectionTo();
+        $db = $db ?? parent::$dbByDefault;
+
+        if (!parent::$initialize) {
+            parent::$initialize = true;
+            parent::credentials();
         }
-        return self::$connection;
+
+        if (!key_exists($db, parent::$connections) || is_null(parent::$connections[$db])) {
+            parent::$connections[$db] = parent::connectionTo($db);
+        }
+
+        return parent::$connections[$db];
     }
 
     /**
      * Desconectar de la DB.
      */
-    public static function disconnect()
+    public static function disconnect($db = null)
     {
-        self::$connection = null;
+        $db = $db ?? parent::$dbByDefault;
+
+        parent::$connections[$db] = null;
     }
 }
