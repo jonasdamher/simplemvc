989eb93c958a7c711d20e2861de26a5855d2dc5b
Jonás <jonas.damher@gmail.com>
2020-06-29 23:09:26 +0100

    clase para comprobación de permisos de usuario en vistas

---
+++ libs/Auth.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Para autenticar y comprobar usuarios que han iniciado sesión.
+ * Para verificar que tienen acceso a una página en concreto.
+ */
+class Auth
+{
+	public function role(string $role = null)
+	{
+		switch ($role) {
+			case 'ROLE_ADMIN':
+
+				if (!isset($_SESSION['userId']) || $_SESSION['userIdRol'] != 1 || $_SESSION['userRolIdentity'] != 'ROLE_ADMIN') {
+					Utils::redirection('home');
+				}
+
+				break;
+			default:
+				if (isset($_SESSION['userId'])) {
+					Utils::redirection('users/profile');
+				}
+				break;
+		}
+	}
+}
