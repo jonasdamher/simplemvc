d70f727a92ca68d0cdebdcb2e7c62098564f229a
Jonás <jonas.damher@gmail.com>
2020-07-19 12:06:32 +0100

    solo una vista para mostrar errores

--- views/error/404.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-	<div class="row">
-		<h1>Error 404. No encontrado.</h1>
-	</div>
-</div>
--- views/error/index.php
+++ views/error/index.php
@@ -1,5 +1,5 @@
 <div class="container">
 	<div class="row">
-		<h1>Error 500. Error interno.</h1>
+		<h1><?= $message ?></h1>
 	</div>
 </div>
