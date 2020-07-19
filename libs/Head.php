16d39597b2f45b1a1b948958f2b9cb572f1e9ac4
Jonás <jonas.damher@gmail.com>
2020-07-19 11:45:23 +0100

    Clases de ayuda al mostrar vista

--- libs/Head.php
+++ libs/Head.php
@@ -10,13 +10,17 @@ class Head
 {
 	private static string $title = '';
 	private static string $description = 'Example page, using PHP with MVC architecture';
+	private static string $keyWords = 'HTML, CSS, JavaScript';
+
+	private static array $linksCss = [];
+	private static array $linksJs = [];
 
 	/**
 	 * Establecer título de página web.
 	 */
 	public static function title(string $title)
 	{
-		self::$title = $title . ' -';
+		self::$title = $title . ' - ';
 	}
 
 	/**
@@ -41,5 +45,51 @@ class Head
 	public static function getDescription(): string
 	{
 		return self::$description;
+	}
+
+	/**
+	 * Añadir keywords al head de la página web.
+	 */
+	public static function keyWords(string $keywords)
+	{
+		self::$keyWords = $keywords;
+	}
+	/**
+	 * Devuelve un array con todas las keywords de la página web.
+	 */
+	public static function getKeyWords(): string
+	{
+		return self::$keyWords;
+	}
+
+	/**
+	 * Añadir un nuevo archivo CSS al head de la página web.
+	 */
+	public static function css(string $fileName)
+	{
+		array_push(self::$linksCss, $fileName);
+	}
+
+	/**
+	 * Devuelve un array con todos los archivos CSS del head.
+	 */
+	public static function getLinksCss(): array
+	{
+		return self::$linksCss;
+	}
+
+	/**
+	 * Añadir un nuevo archivo JavaScript al head de la página web.
+	 */
+	public static function js(string $fileName)
+	{
+		array_push(self::$linksJs, $fileName);
+	}
+	/**
+	 * Devuelve un array con todos los archivos JavaScript del head.
+	 */
+	public static function getLinksJs(): array
+	{
+		return self::$linksJs;
 	}
 }
