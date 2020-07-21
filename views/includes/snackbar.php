e2bcecc53928a0e0a024a4821887cbcb413ddada
Jonás <jonas.damher@gmail.com>
2020-07-21 20:17:51 +0100

    icono cerrar añadido

--- views/includes/snackbar.php
+++ views/includes/snackbar.php
@@ -1,7 +1,7 @@
 <?php if ($this->getResponseModel()) { ?>
 	<div class="snackbar shadow-sm">
 		<div class="snackbar-body">
-			<p><?= $this->getResponseModel() ?></p><button type="button" class="btn text-danger ml-2 btn-snackbar-close" title="close">X</button>
+			<p><?= $this->getResponseModel() ?></p><button type="button" class="btn text-danger ml-2 btn-snackbar-close" title="close"><i class="fas fa-times"></i></button>
 		</div>
 	</div>
 <?php } ?>
