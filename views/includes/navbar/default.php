8507ded95ad4909ce607f3550e2beb506fa95704
Jonás <jonas.damher@gmail.com>
2020-07-21 18:54:20 +0100

    modificando navbar por defecto

--- views/includes/navbar/default.php
+++ views/includes/navbar/default.php
@@ -1,10 +1,10 @@
 <div class="navbar-content">
 	<a href="<?= URL_BASE ?>home">
-		<img class="logo" src="<?= URL_BASE ?>public/img/logo-simplymvcphp.png" title="logo simplymvcphp" alt="logo simplymvcphp" />
+		<img class="logo" src="<?= URL_BASE ?>public/img/logo-simplymvcphp.png" title="logo" alt="logo" />
 	</a>
 	<ul class="navbar-menu">
 		<li class="<?= Utils::menuActive('home') ?>"><a href="<?= URL_BASE ?>home"><span><i class="fas fa-home"></i></span>Home</a></li>
 		<li class="<?= Utils::menuActive('articles') ?>"><a href="<?= URL_BASE ?>articles"><span><i class="fas fa-tags"></i></span>Articles</a></li>
-		<li class="<?= Utils::menuActive('articles') ?>"><button type="button" class="btn"><i class="fas fa-search fa-lg"></i></button></li>
+		<li class="<?= Utils::menuActive('articles') ?>"><button type="button" class="btn" title="Search"><i class="fas fa-search fa-lg"></i></button></li>
 	</ul>
 </div>
