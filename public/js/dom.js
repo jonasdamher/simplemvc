9b77c0dbb415b9b62c8af049b89682ae6ffc3fca
Jonás <jonas.damher@gmail.com>
2020-08-02 17:31:54 +0100

    modo estricto añadido, animación de buscador de navbar

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function modalRemoveShowData(data, modal) {
   if (!data.success) {
     $("#error-request-remove").show();
@@ -9,6 +11,7 @@ function modalRemoveShowData(data, modal) {
   $("#btn-remove-category").val(data.result.id);
   $(modal).show();
 }
+
 function modalRemove(data, id) {
   if (!data.success) {
     $("#error-request-remove").show();
@@ -19,6 +22,23 @@ function modalRemove(data, id) {
   $("#request-success-remove").show();
   $("#request-success-remove").text("Success, Remove category !");
   $("#table-categories").child("tbody").child(`tr[data-id=${id}]`).remove();
+}
+
+function statusInputSearch() {
+  $("#btn-quit-search").toggleClass("d-none");
+
+  if ($("#search").hasClass("d-none")) {
+    $("#search")
+      .toggleClass("d-none")
+      .removeClass("search-hide")
+      .toggleClass("search-show");
+    return;
+  }
+
+  $("#search").removeClass("search-show").toggleClass("search-hide");
+  setTimeout(() => {
+    $("#search").toggleClass("d-none");
+  }, 300);
 }
 
 $(function () {
@@ -32,6 +52,19 @@ $(function () {
     } else {
       btn.children("svg").removeClass("fa-caret-up").addClass("fa-caret-down");
     }
+  });
+
+  $("#btn-search").click(function () {
+    let btn = $(this);
+    if (btn.attr("data-toggle") == "close") {
+      statusInputSearch();
+    }
+    btn.attr("data-toggle", "open");
+  });
+
+  $("#btn-quit-search").click(function () {
+    $("#btn-search").attr("data-toggle", "close");
+    statusInputSearch();
   });
 
   $(".btn-snackbar-close").click(function () {
