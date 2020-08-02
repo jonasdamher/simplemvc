526e37b4e73fba33beffec99defb263e4bd5e789
Jonás <jonas.damher@gmail.com>
2020-08-02 19:13:29 +0100

    cambio de estilo de botones buscados

--- views/includes/navbar/default.php
+++ views/includes/navbar/default.php
@@ -10,9 +10,9 @@
 <section class="navbar-content">
 	<ul class="navbar-menu m-0">
 		<li class="m-0">
-			<button type="button" id="btn-search" class="btn btn-square" title="Search" data-toggle="close"><i class="fas fa-search fa-lg"></i></button>
+			<button type="button" id="btn-search" class="btn btn-secondary btn-square" title="Search" data-toggle="close"><i class="fas fa-search fa-lg"></i></button>
 			<input type="search" id="search" class="input d-none" placeholder="Search..." />
-			<button type="button" id="btn-quit-search" class="btn btn-square d-none" title="Search quit"><i class="fas fa-times fa-lg"></i></button>
+			<button type="button" id="btn-quit-search" class="btn btn-secondary btn-square d-none" title="Search quit"><i class="fas fa-times fa-lg"></i></button>
 		</li>
 	</ul>
 </section>
