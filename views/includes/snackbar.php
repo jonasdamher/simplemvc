c8c3690b5090b20443612879e4b49a70a99dedd0
Jonás <jonas.damher@gmail.com>
2020-08-02 19:12:45 +0100

    cambio botón

--- views/includes/snackbar.php
+++ views/includes/snackbar.php
@@ -1,7 +1,7 @@
 <?php if ($this->getResponseModel()) { ?>
 	<div class="snackbar shadow-sm">
 		<div class="snackbar-body">
-			<p><?= $this->getResponseModel() ?></p><button type="button" class="btn text-danger ml-2 btn-snackbar-close" title="close"><i class="fas fa-times"></i></button>
+			<p><?= $this->getResponseModel() ?></p><button type="button" class="btn text-danger ml-2 btn-snackbar-close btn-square" title="close"><i class="fas fa-times"></i></button>
 		</div>
 	</div>
 <?php } ?>
