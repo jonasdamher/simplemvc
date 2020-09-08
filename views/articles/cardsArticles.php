e0505356332979fb643411d22c473edbb5579b45
Jonás <jonas.damher@gmail.com>
2020-09-08 11:12:37 +0100

    modularización

---
+++ views/articles/cardsArticles.php
@@ -0,0 +1,16 @@
+<?php
+if ($articles['success']) {
+	foreach ($articles['result'] as $article) { ?>
+		<article class="card bg-light shadow-md">
+			<div class="card-body">
+				<p class="card-text-lead"><a href="<?= URL_BASE . 'articles/get/' . $article['urlName'] ?>"><?= $article['title'] ?></a></p>
+				<p><?= $article['description'] ?></p>
+				<p><?= $article['user'] ?></p>
+				<a href="categories/get/<?= $article['idCategory'] ?>"><?= $article['categoryName'] ?></a>
+				<p><?= $article['createAt'] ?></p>
+			</div>
+		</article>
+	<?php }
+} else { ?>
+	<p><?= $this->getResponseModel() ?></p>
+<?php } ?>
