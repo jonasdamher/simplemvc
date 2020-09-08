6d85bd317f09bd5f2191136326aadc338e8556b5
Jonás <jonas.damher@gmail.com>
2020-09-08 13:57:31 +0100

    método para comprobar $_SESSION

--- libs/Auth.php
+++ libs/Auth.php
@@ -13,7 +13,7 @@ class Auth
 		switch ($role) {
 			case 'ROLE_ADMIN':
 
-				if (!isset($_SESSION['userId']) || $_SESSION['userIdRol'] != 1 || $_SESSION['userRolIdentity'] != 'ROLE_ADMIN') {
+				if (!$this->verify() || !($_SESSION['userIdRol'] == 1) || !($_SESSION['userRolIdentity'] == 'ROLE_ADMIN')) {
 					Utils::redirection('error/401');
 				}
 
@@ -23,6 +23,65 @@ class Auth
 					Utils::redirection('users/profile');
 				}
 				break;
+		}
+	}
+
+	private function checkSession()
+	{
+		$list = ['userId', 'userName', 'userRolName', 'userIdRol', 'userRolIdentity'];
+		$countSession = count($_SESSION);
+		$ok = true;
+
+		if ($countSession > 0) {
+			foreach ($list as $key) {
+				if (!array_key_exists($key, $_SESSION)) {
+					$ok = false;
+					break;
+				}
+			}
+		} else {
+			$ok = false;
+		}
+
+		return $ok;
+	}
+
+	private function verify()
+	{
+		$ok = false;
+		try {
+
+			if (!$this->checkSession()) {
+				throw new Exception(false);
+			}
+
+			$consult = Database::connect()->prepare('SELECT 
+			user.id 
+			FROM users as user 
+			INNER JOIN usr_rol ON user.idRol = usr_rol.id 
+			WHERE user.id =:userId and user.name = :userName and 
+			usr_rol.rol=:userRolName and usr_rol.id=:userIdRol and usr_rol.indentity=:userRolIdentity');
+
+			$consult->bindValue(':userId',	$_SESSION['userId'], PDO::PARAM_INT);
+			$consult->bindValue(':userName',	$_SESSION['userName'], PDO::PARAM_STR);
+			$consult->bindValue(':userRolName',	$_SESSION['userRolName'], PDO::PARAM_STR);
+			$consult->bindValue(':userIdRol',	$_SESSION['userIdRol'], PDO::PARAM_INT);
+			$consult->bindValue(':userRolIdentity',	$_SESSION['userRolIdentity'], PDO::PARAM_STR);
+
+			$consult->execute();
+
+			if ($consult->rowCount() == 0) {
+				throw new Exception(false);
+			}
+
+			$ok = true;
+		} catch (PDOException $e) {
+			$ok = false;
+		} catch (Exception $e) {
+			$ok = false;
+		} finally {
+
+			return $ok;
 		}
 	}
 }
