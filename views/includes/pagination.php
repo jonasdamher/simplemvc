00c745db4e0998ad0a7e8fefeff94af1f30d3d31
Jonás <jonas.damher@gmail.com>
2020-09-08 11:11:21 +0100

    add pagination

---
+++ views/includes/pagination.php
@@ -0,0 +1,22 @@
+<div class="row">
+	<div class="col-12 d-flex j-end pb-1">
+		<section class="pagination">
+			<div class="pagination-body">
+				<nav role="navigation" aria-label="Pagination Navigation">
+					<ul>
+						<?php foreach ($pagination['pagination'] as $page) { ?>
+							<li>
+								<a class="<?= $page['active'] ? 'active' : '' ?><?= $page['disabled'] ? ' disabled' : '' ?>" <?= $page['active'] ? 'aria-current="true"' : '' ?> aria-label="<?= $page['active'] ? 'Current Page, Page ' . $page['page'] : 'Go to page ' . $page['page'] ?>" title="<?= $page['active'] ? 'Current Page, page ' . $page['page'] : 'Go to page ' . $page['page'] ?>" <?= (!empty($page['rel']) ? 'rel="' . $page['rel'] . '"' : '') ?> href="<?= URL_BASE . 'articles?page=' . $page['page'] . '&limit=' . $limit ?>">
+									<?= $page['page'] ?>
+								</a>
+							</li>
+						<?php } ?>
+					</ul>
+				</nav>
+			</div>
+		</section>
+	</div>
+	<div class="col-12 d-flex j-end">
+		<p>Page <?= $pagination['current'] ?> of <?= $pagination['pages'] ?></p>
+	</div>
+</div>
