48eeed378e5be8dacc3259e3bd8847342c592db8
Jonás <jonas.damher@gmail.com>
2020-09-22 20:18:49 +0100

    add categories class

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,5 +1,7 @@
 "use strict";
 
+class categories extends validator {}
+
 $(function () {
   // OPEN MODAL - NEW
   $(".btn-modal-new").on("click", function () {
@@ -7,20 +9,22 @@ $(function () {
   });
 
   // OPEN MODAL - REMOVE
-  $(".btn-modal-remove").on("click", function () {
+  $("#table-categories").on("click", ".btn-modal-remove", function () {
     const id = $(this).parent().parent().data("id");
+    const category = new categories();
 
-    get("getCategory/" + id).done(function (json) {
+    category.get("categories/get/" + id).done(function (json) {
       modalRemoveShowData(JSON.parse(json), "#modal-remove");
     });
   });
-  // REMOVE CATEGORY
-  $("#btn-remove-category").click(function () {
-    const id = $(this).val();
 
-    get("deleteCategory/" + id).done(function (json) {
-      var data = JSON.parse(json);
-      modalRemove(data, id);
-    });
-  });
+  // REMOVE CATEGORY
+  // $("#btn-remove-category").click(function () {
+  //   const id = $(this).val();
+
+  //   get("deleteCategory/" + id).done(function (json) {
+  //     var data = JSON.parse(json);
+  //     modalRemove(data, id);
+  //   });
+  // });
 });
