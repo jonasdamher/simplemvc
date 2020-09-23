6e103d850ae019680f72546ae53ee966366ca529
Jonás <jonas.damher@gmail.com>
2020-09-23 20:30:07 +0100

    add class for max width card

--- views/articles/cardsArticles.php
+++ views/articles/cardsArticles.php
@@ -1,9 +1,9 @@
 <?php
 if ($articles['success']) {
 	foreach ($articles['result'] as $article) { ?>
-		<article class="card bg-light shadow-md">
+		<article class="card bg-light shadow-md mw-card">
 			<div class="card-body">
-				<p class="card-text-lead"><a href="<?= URL_BASE . 'articles/get/' . $article['urlName'] ?>"><?= $article['title'] ?></a></p>
+				<p class="card-text-lead"><a href="<?= URL_BASE . 'articles/' . $article['urlName'] ?>"><?= $article['title'] ?></a></p>
 				<p><?= $article['description'] ?></p>
 				<p><?= $article['user'] ?></p>
 				<a href="categories/get/<?= $article['idCategory'] ?>"><?= $article['categoryName'] ?></a>
