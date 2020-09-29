9c317ccd775166a124500de9b3508cf4294636c1
Jonás <jonas.damher@gmail.com>
2020-09-29 21:44:22 +0100

    remove request jquery, now using fetch

--- public/js/request.js
+++ public/js/request.js
@@ -1,19 +1,60 @@
 "use strict";
 
 class request {
-  post(route,  formData ) {
-    formData._token='';
-    const form = JSON.stringify(formData);
-    console.log(form);
-    return $.post("/api/" + route, { form: form }, json => {
-      return json;
+  
+  async get(url = '', data = {}) {
+    data._token = '';
+    const response = await fetch('/api/'+url, {
+      method: 'POST',
+      cache: 'no-cache',
+      referrerPolicy: 'strict-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     });
+    return response.json();
+  }
+  
+  async post(url = '', data = {}) {
+    data._token = '';
+    const response = await fetch('/api/'+url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      cache: 'no-cache',
+      referrerPolicy: 'strict-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.text();
+  }
+  
+  async delete(url = '', data = {}) {
+    data._token = '';
+    const response = await fetch('/api/'+url, {
+      method: 'DELETE',
+      cache: 'no-cache',
+      referrerPolicy: 'strict-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
   }
 
-  get(route) {
-    const form = JSON.stringify({ _token: "" });
-    return $.post("/api/" + route, { form: form }, json => {
-      return json;
+  async put(url = '', data = {}) {
+    data._token = '';
+    const response = await fetch('/api/'+url, {
+      method: 'PUT',
+      cache: 'no-cache',
+      referrerPolicy: 'strict-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     });
+    return response.json();
   }
 }
