4280be3e8dd29242755324d9aa45727e18ef1426
Jonás <jonas.damher@gmail.com>
2020-09-29 21:42:23 +0100

    modularize cotroller methods

---
+++ api/ArticlesApi.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+class ArticlesApi extends BaseController
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->modelLoading(['articles', 'tags', 'categories']);
+	}
+
+	public function apiCreate()
+	{
+		$this->auth->role('ROLE_ADMIN');
+
+		$json = $this->json->rawRequest();
+
+		// Pasarle al modelo de artículo los datos recogidos por POST
+		$this->model('articles')->setTitle($json->title);
+		$this->model('articles')->setDescription($json->description);
+		$this->model('articles')->setMain($json->main);
+		$this->model('articles')->setIdUser($_SESSION['userId']);
+		$this->model('articles')->setIdCategory($json->category);
+		$this->model('articles')->setUrlName($json->title);
+
+		$create = $this->model('articles')->create();
+
+		if ($create['success'] && count($json->tags) > 0) {
+
+			// Añadir etiquetas si es que las tiene.
+
+			foreach ($json->tags as $tag) {
+				$this->model('tags')->setIdArticle($create['result']['lastId']);
+				$this->model('tags')->setName($tag);
+
+				$this->model('tags')->create();
+			}
+		}
+
+		$this->json->jsonResponse($create);
+	}
+}
