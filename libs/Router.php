9eced04934dd7baaf09447fdbf84adacbf224337
Jonás <jonas.damher@gmail.com>
2020-09-29 21:41:42 +0100

    update for api or view

--- libs/Router.php
+++ libs/Router.php
@@ -15,8 +15,31 @@ $action = strtolower(trim($_GET['action'] ?? 'index'));
 
 $api = $_GET['api'] ?? null;
 
+$path = '';
+$className = '';
+
 if (!is_null($api) && $api == 'api') {
+
+    require_once 'libs/JsonRequest.php';
+
+    require_once 'core/ApiController.php';
+
+    // Autocargar todas las clases api
+    require_once 'libs/ApiAutoload.php';
+
     $action = 'api' . ucfirst($action);
+    $path = 'api';
+    $className = $controller . 'Api';
+} else {
+
+    require_once 'helpers/View.php';
+
+    require_once 'core/Controller.php';
+
+    // Autocargar todas las clases
+    require_once 'libs/ControllerAutoload.php';
+    $path = 'controllers';
+    $className = $controller . 'Controller';
 }
 
 if ($controller == 'error') {
@@ -27,9 +50,8 @@ if ($controller == 'error') {
  * Comprueba que existe el archivo y la clase controlador
  * e instancia el controlador.
  */
-$className = $controller . 'Controller';
 
-if (!file_exists('controllers/' . $className . '.php')) {
+if (!file_exists($path . '/' . $className . '.php')) {
     Utils::redirection('error/500');
 }
 
