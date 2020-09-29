0b11836cb101ca02fd471411d6a76c4fd293d649
Jonás <jonas.damher@gmail.com>
2020-09-29 21:41:16 +0100

    add button for open menu on media

--- views/includes/navbar/default.php
+++ views/includes/navbar/default.php
@@ -1,8 +1,8 @@
 <section class="navbar-content">
-	<a href="<?= URL_BASE ?>home">
-		<img class="logo" src="<?= URL_BASE ?>public/images/logo/launcher-1.webp" title="logo" alt="logo" />
+	<a class="logo" href="<?= URL_BASE ?>home" title="Go to home">
+		<img src="<?= URL_BASE ?>public/images/logo/launcher-1.webp" title="jonasdamher" alt="jonasdamher" />
 	</a>
-	<ul class="navbar-menu">
+	<ul id="main-menu" class="navbar-menu">
 		<li class="<?= Utils::menuActive('home') ?>"><a href="<?= URL_BASE ?>home"><span><i class="fas fa-home"></i></span>Home</a></li>
 		<li class="<?= Utils::menuActive('articles') ?>"><a href="<?= URL_BASE ?>articles"><span><i class="fas fa-tags"></i></span>Articles</a></li>
 	</ul>
@@ -10,7 +10,7 @@
 <section class="navbar-content">
 	<form action="<?= URL_BASE ?>articles/search" method="get">
 		<ul class="navbar-menu m-0">
-			<li class="m-0">
+			<li class="m-0 d-flex items-center">
 				<label for="search" title="Search">
 					<button type="<?= (empty(Utils::getValue('q')) ? 'button' : 'submit') ?>" id="btn-search" class="btn btn-secondary btn-square" title="Search" data-toggle="close">
 						<i class="fas fa-search fa-lg"></i>
@@ -26,4 +26,7 @@
 			</li>
 		</ul>
 	</form>
+	<button type="button" id="btn-main-menu" class="btn btn-secondary btn-square" title="Main menu">
+		<i class="fas fa-bars fa-lg"></i>
+	</button>
 </section>
