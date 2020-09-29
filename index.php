07550e7511539cc6b067f5fe1205276efff75f3b
Jonás <jonas.damher@gmail.com>
2020-09-29 21:46:01 +0100

    remove includes

--- index.php
+++ index.php
@@ -17,17 +17,13 @@ session_start();
 // Variables globales
 require_once 'conf/Globals.php';
 
-// Autocargar todas las clases
-require_once 'libs/autoload.php';
-
 // Clases ayudantes
 require_once 'helpers/Utils.php';
-require_once 'helpers/View.php';
+
 require_once 'core/Database.php';
 
 require_once 'libs/sitemap.php';
 require_once 'libs/Auth.php';
-require_once 'libs/JsonRequest.php';
 
 // Clases principales de la webapp
 require_once 'core/Database.php';
